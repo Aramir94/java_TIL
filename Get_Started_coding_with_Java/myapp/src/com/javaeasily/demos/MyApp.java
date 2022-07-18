280a799913a65af4a98901f921233c7bcb197b7e
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-07-18 13:39:30 +0900

    hello world project

---
+++ Get_Started_coding_with_Java/myapp/src/com/javaeasily/demos/MyApp.java
@@ -0,0 +1,13 @@
+package com.javaeasily.demos;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		sayHello("Flycode77");
+	}
+
+	private static void sayHello(String name) {
+		System.out.println("HEllO " + name);
+	}
+
+}
